dcab82a9be6feb8ee0b0bcdb3976e01ae32d4860
FionaViola <aditi.nadig10@gmail.com>
2018-12-13 21:34:42 -0500

    Updates

---
+++ precache-manifest.00b79e1eee4e153759cfecca5c1ceb02.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f0e0185359dfcf516ba",
+    "url": "/DifferOrDie-MemoryGame/static/css/main.e02b5e7c.chunk.css"
+  },
+  {
+    "revision": "2f0e0185359dfcf516ba",
+    "url": "/DifferOrDie-MemoryGame/static/js/main.2f0e0185.chunk.js"
+  },
+  {
+    "revision": "ad02920dda1451a43201",
+    "url": "/DifferOrDie-MemoryGame/static/js/1.ad02920d.chunk.js"
+  },
+  {
+    "revision": "4f30aecd2c12aba74b82",
+    "url": "/DifferOrDie-MemoryGame/static/js/runtime~main.4f30aecd.js"
+  },
+  {
+    "revision": "1b892e5674474a039215f8af3b27cc89",
+    "url": "/DifferOrDie-MemoryGame/static/media/background1.1b892e56.png"
+  },
+  {
+    "revision": "4baca589f5d11489d265fb84f265b371",
+    "url": "/DifferOrDie-MemoryGame/static/media/bart.4baca589.jpg"
+  },
+  {
+    "revision": "859e016dd3981f0a0e59c98f05231d75",
+    "url": "/DifferOrDie-MemoryGame/static/media/chris.859e016d.jpg"
+  },
+  {
+    "revision": "0b16cd69b3d9b3d2f073e104a413e7f2",
+    "url": "/DifferOrDie-MemoryGame/static/media/crazy.0b16cd69.jpg"
+  },
+  {
+    "revision": "b6d7509879dd3dbad15616984533f6f3",
+    "url": "/DifferOrDie-MemoryGame/static/media/homer.b6d75098.jpg"
+  },
+  {
+    "revision": "a745f08cebc8ff9e706460b59f22719a",
+    "url": "/DifferOrDie-MemoryGame/static/media/lisa.a745f08c.png"
+  },
+  {
+    "revision": "94163c33b07fb80502bc6057ada9504f",
+    "url": "/DifferOrDie-MemoryGame/static/media/maggie.94163c33.jpg"
+  },
+  {
+    "revision": "4c51eaf461e4eeefeb6fc9064d1ae9c1",
+    "url": "/DifferOrDie-MemoryGame/static/media/moe.4c51eaf4.jpg"
+  },
+  {
+    "revision": "5cf24bd3d42b17e0ba491686c8aab6cf",
+    "url": "/DifferOrDie-MemoryGame/static/media/selma.5cf24bd3.jpg"
+  },
+  {
+    "revision": "1501acf37d8ba81f9c676661a32ce42b",
+    "url": "/DifferOrDie-MemoryGame/index.html"
+  }
+];
